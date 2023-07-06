30ac820735284fdd64b1f3bbb76d177dff049de2
captbay <agespramana9@gmail.com>
2023-07-06 15:38:34 +0700

    init template web kkn

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-500 py-4">
+      <div className="container mx-auto px-4">
+        <div className="mt-4 text-center">
+          <p className="text-white">
+            &copy; {new Date().getFullYear()} KKN UAJY Butuh Lor (Kelompok 53)
+            by I Putu Agestya Pramana. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
