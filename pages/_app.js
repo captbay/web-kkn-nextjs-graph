24e7da3cdeaec1a993333e8465966d12f9d4d8b7
captbay <agespramana9@gmail.com>
2023-07-07 14:25:04 +0700

    Web Analytics in Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
-import '../styles/globals.scss';
-import { Layout } from '../components';
+import "../styles/globals.scss";
+import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
